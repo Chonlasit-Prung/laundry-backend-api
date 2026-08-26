586dcdf2a12f0ca99e0f0f017c8834efa0801161
flukeer <chonlasit.work1@gmail.com>
2026-08-26 07:28:19 +0700

    Fix status 139 crash by disabling BCrypt native optimization

--- obj/Debug/net10.0/LaundryApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/LaundryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaundryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6cf5d62628787680bf6ca1b4ea78aa7fb101a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaundryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaundryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
